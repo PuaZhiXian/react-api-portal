df0a98d00b0ceea82c8ecd65557558b60c697ad4
PuaZhiXian <xianpua2001@gmail.com>
2023-11-25 15:43:38 +0800

    add personal information tab

--- src/app/pages/profile/personalInformation.tsx
+++ src/app/pages/profile/personalInformation.tsx
@@ -1,5 +1,61 @@
+import {Input} from "@nextui-org/input";
+import {Button} from "@nextui-org/react";
+import React, {useState} from "react";
+import {IPersonalInformation} from "@/interface/i-personal-information";
+
 export default function PersonalInformation() {
+
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+
+    const [personalInformation, setPersonalInformation] = useState<IPersonalInformation>(
+        {email: "zhixianpua2001@gmail.com", organisation: "Um Library", username: "Pua Zhi Xian"}
+    )
+
+    const saveProfile = () => {
+        console.log('saving update')
+        setIsEdit(false);
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        personalInformation[name] = value;
+        setPersonalInformation(values => ({...values, [name]: value}))
+    };
+
     return (
-        <h1>Personal information</h1>
+        <div>
+            <form method="POST">
+                <div className="my-3">
+                    <Input type="text" isReadOnly={!isEdit} onChange={handleChange} name="username"
+                           label="Username" value={personalInformation.username} className="max-w-xs"/>
+                </div>
+                <div className="my-3">
+                    <Input type="email" isReadOnly name="email"
+                           label="Email" value={personalInformation.email} className="max-w-xs"/>
+                </div>
+                <div className="my-3">
+                    <Input type="text" isReadOnly name="organisation"
+                           label="Organisation" value={personalInformation.organisation} className="max-w-xs"/>
+                </div>
+
+                <div className="mt-4 primary-button">
+                    {
+                        !isEdit &&
+                        <Button color={'primary'} className=" font-bold text-white
+                            text-center rounded-lg" onClick={() => setIsEdit(true)}>
+                            Update Profile
+                        </Button>
+                    }
+                    {
+                        isEdit &&
+                        <Button color={'primary'} className=" font-bold text-white
+                            text-center rounded-lg" onClick={() => saveProfile()}>
+                            Save
+                        </Button>
+                    }
+                </div>
+
+            </form>
+        </div>
     )
 }
---
+++ src/interface/i-personal-information.ts
@@ -0,0 +1,5 @@
+export interface IPersonalInformation {
+  username: string;
+  email: string;
+  organisation: string;
+}
